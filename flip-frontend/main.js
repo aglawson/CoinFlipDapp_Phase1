1ae83155d0630a884c423fb47333e4df1c185c90
Adrian Lawson <adrianboblawson@csu.fullerton.edu>
2021-02-12 10:34:33 -0800

    Completed front end

--- flip-frontend/main.js
+++ flip-frontend/main.js
@@ -3,20 +3,21 @@ var contractInstance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-        contractInstance = new web3.eth.Contract(abi, "0xc9b839b8D91f9e222b8C167e4E57577CCC807958", {from: accounts[0]});
+        contractInstance = new web3.eth.Contract(abi, "0x2c0A366D241561762DD48B71f7Ee26B4fCeB7215", {from: accounts[0]});
     });
     
     $("#flip_button").click(flip);
     $("#fund_contract_button").click(fundContract);
     $("#withdraw_button").click(withdrawBalance);
+    $("#balance_button").click(getBalance);
 });
+
 
 function flip(){
     var wager = $("#bet_input").val();
-    // var config = {
-    //     value: web3.utils.toWei(wager, "ether")
-    // };
-    contractInstance.methods.placeBet().send({value: web3.utils.toWei(wager,"ether")})
+    var bet = "" + wager;
+    
+    contractInstance.methods.placeBet().send({value: web3.utils.toWei(bet,"ether")})
     .on("transactionHash", function(hash){
         console.log(hash);
     })
@@ -25,22 +26,22 @@ function flip(){
     })
     .on("receipt", function(receipt){
         console.log(receipt);
-        if(receipt.events.placeBet.returnValues[2] === false){
+        if(receipt.events.placeBet.returnValues[2] == false){
             alert("You lost" + wager + " Ether");
-        }else if(receipt.events.placeBet.returnValues[2] === true){
+        }else if(receipt.events.placeBet.returnValues[2] == true){
             alert("You won " + wager + " Ether!");
         }
     })
 }
 
 function fundContract() {
-    var addAmount = $("#func_input").val();
-    
+    var addAmount = $("#fund_input").val();
+    var amt = "" + addAmount;
     var config = {
-        value: web3.utils.toWei(addAmount,"ether")
+        value: web3.utils.toWei(amt,"ether")
     }
 
-    contractInstance.methods.fundContract().send(config)
+    contractInstance.methods.fundContract().send({value: web3.utils.toWei(amt,"ether")})
     .on("transactionHash", function(hash){
         console.log(hash);
     })
